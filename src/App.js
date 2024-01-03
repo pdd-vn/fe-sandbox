25cfb53092109050d21b1705e46cecb2e62d0160
pdd-vn <dungphamdang99@gmail.com>
2024-01-03 17:33:25 +0700

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,89 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react'
+import { MetaMaskButton } from "@metamask/sdk-react-ui";
 
 function App() {
+  const nftImages = Array(10).fill('https://cdn-icons-png.flaticon.com/512/4155/4155897.png');
+  const nftPrices = Array(10).fill(666);
+
+  const NFTImage = ({ nft_img_url }) => {
+    const [hover, setHover] = useState(false);
+    const handleMouseOver = () => {
+      setHover(true);
+    };
+
+    const handleMouseOut = () => {
+      setHover(false);
+    };
+
+    return (
+      <div className="nft-card" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
+        <img src={nft_img_url} alt="NFT" className="nft-image" />
+        {/* {
+          true && (
+            <div className='nft-info'>
+              Only visible when hovering div
+            </div>
+          )
+        } */}
+        <div className='nft-info'>
+          <div className='info-container'>
+            <div className='info-item'>
+              <div className='info-key'>Name:</div><div className='info-value'>Dung</div>
+            </div>
+            <div className='info-item'>
+              <div className='info-key'>Age:</div><div className='info-value'>10</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
+
+  const NFTCard = ({ nft_img_url, nft_price, onLend, onClaim }) => {
+    return (
+      <>
+        <NFTImage nft_img_url={nft_img_url} />
+        <div className="button-container">
+          <button onClick={onLend} className="lend-button">
+            Lend {nft_price} ETH
+          </button>
+          <button onClick={onClaim} className="claim-button">
+            Claim
+          </button>
+        </div>
+      </>
+    );
+  };
+
+  const handleLend = (nftId) => {
+    console.log(`Buy button clicked for NFT ${nftId}`);
+    // Add your logic for buying the specific NFT here
+  };
+
+  const handleClaim = (nftId) => {
+    console.log(`Like button clicked for NFT ${nftId}`);
+    // Add your logic for liking the specific NFT here
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" >
+      <div className="metamask-button">
+        <MetaMaskButton theme={"light"} color="white"></MetaMaskButton>
+      </div>
+      <div className="nft-gallery">
+        {nftImages.map((imgUrl, index) => (
+          <div key={index} className="nft-frame">
+            <NFTCard
+              nft_img_url={imgUrl}
+              nft_price={nftPrices[index]}
+              onLend={() => handleLend(index)}
+              onClaim={() => handleClaim(index)}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { MetaMaskUIProvider } from '@metamask/sdk-react-ui';
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+  <React.StrictMode>
+    <MetaMaskUIProvider sdkOptions={{
+      dappMetadata: {
+        name: "Example React UI Dapp",
+        url: window.location.host,
+      }
+      // Other options
+    }}>
+      <App />
+    </MetaMaskUIProvider>
+  </React.StrictMode>
+);
