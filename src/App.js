c68b3ba8b807294010db47ed875064e3cb19b340
pdd-vn <dungphamdang99@gmail.com>
2024-01-07 15:35:22 +0700

    WIP

--- src/App.js
+++ src/App.js
@@ -1,61 +1,48 @@
-import './App.css';
-import React, { useState } from 'react'
-import { MetaMaskButton } from "@metamask/sdk-react-ui";
+import './css/App.css';
+import React, { useState, useEffect } from 'react'
+import {
+  MetaMaskButton, useAccount,
+  useSDK,
+  useSignMessage
+} from '@metamask/sdk-react-ui';
+import {
+  NFTCard
+} from './nft.js';
+const ethers = require("ethers");
+const typechain = require("nft-lending-machine");
+
+// Sepolia testnet
+// const owner_addr = "0xf505B2b47BaC8849584915588bA3C0a01bd72206"
+// const erc20_addr = "0x27566bEb67F55a12860bc3DaA50fF57B3dE76183"
+// const machine_addr = "0x6168c921c5425859F64a21940bC9841521cf0284"
+
+// Local testnet
+const owner_addr = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+const erc20_addr = "0x4ed7c70F96B99c776995fB64377f0d4aB3B0e1C1"
+const machine_addr = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
 
 function App() {
-  const nftImages = Array(10).fill('https://cdn-icons-png.flaticon.com/512/4155/4155897.png');
-  const nftPrices = Array(10).fill(666);
+  let mint_queue = [];
+  const MintNFT = () => {
+    const [uri, setUri] = useState("");
 
-  const NFTImage = ({ nft_img_url }) => {
-    const [hover, setHover] = useState(false);
-    const handleMouseOver = () => {
-      setHover(true);
-    };
+    const handleSubmit = async (event) => {
+      event.preventDefault();
+      mint_queue.push(uri)
+    }
 
-    const handleMouseOut = () => {
-      setHover(false);
-    };
-
-    return (
-      <div className="nft-card" onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>
-        <img src={nft_img_url} alt="NFT" className="nft-image" />
-        {/* {
-          true && (
-            <div className='nft-info'>
-              Only visible when hovering div
-            </div>
-          )
-        } */}
-        <div className='nft-info'>
-          <div className='info-container'>
-            <div className='info-item'>
-              <div className='info-key'>Name:</div><div className='info-value'>Dung</div>
-            </div>
-            <div className='info-item'>
-              <div className='info-key'>Age:</div><div className='info-value'>10</div>
-            </div>
-          </div>
+    return <>
+      <div className="mint-container">
+        <div className="mint-form">
+          <form onSubmit={handleSubmit}>
+            <label>
+              <input type="text" value={uri} onChange={(e) => setUri(e.target.value)} className='mint-uri-text-box' />
+            </label>
+            <input type="submit" value="Mint NFT" className="mint-button" />
+          </form>
         </div>
-      </div>
-    )
+      </div></>
   }
-
-
-  const NFTCard = ({ nft_img_url, nft_price, onLend, onClaim }) => {
-    return (
-      <>
-        <NFTImage nft_img_url={nft_img_url} />
-        <div className="button-container">
-          <button onClick={onLend} className="lend-button">
-            Lend {nft_price} ETH
-          </button>
-          <button onClick={onClaim} className="claim-button">
-            Claim
-          </button>
-        </div>
-      </>
-    );
-  };
 
   const handleLend = (nftId) => {
     console.log(`Buy button clicked for NFT ${nftId}`);
@@ -67,19 +54,45 @@ function App() {
     // Add your logic for liking the specific NFT here
   };
 
+  useEffect(() => {
+    getNFTsList();
+  }, []);
+
+  const [listNft, setListNft] = useState([]);
+  const getNFTsList = async () => {
+    try {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const machine = typechain.LendingMachine__factory.connect(machine_addr, provider);
+
+      const nftList = await machine.list_nfts();
+      let list = [];
+      nftList.forEach(async (e) => {
+        list.push({
+          img_url: await machine.tokenURI(e.token_id),
+          price: (e.is_deposited) ? e.price : null,
+          token_id: e.token_id
+        })
+        setListNft(prev => [...list]);
+      });
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <div className="App" >
-      <div className="metamask-button">
+      <div className="function-bar-container">
+        <MintNFT />
         <MetaMaskButton theme={"light"} color="white"></MetaMaskButton>
       </div>
       <div className="nft-gallery">
-        {nftImages.map((imgUrl, index) => (
+        {listNft.map((nft, index) => (
           <div key={index} className="nft-frame">
             <NFTCard
-              nft_img_url={imgUrl}
-              nft_price={nftPrices[index]}
-              onLend={() => handleLend(index)}
-              onClaim={() => handleClaim(index)}
+              nft_img_url={nft.img_url}
+              nft_price={nft.price}
+              onLend={() => handleLend(nft.token_id)}
+              onClaim={() => handleClaim(nft.token_id)}
             />
           </div>
         ))}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { MetaMaskUIProvider } from '@metamask/sdk-react-ui';
-import './index.css'
+import './css/index.css'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
